e5ed6b08454afd00c3c913982e841baab89513ec
Vibesdk <vibesdk-bot@cloudflare.com>
2025-12-12 17:57:05 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add @dnd-kit/utilities@^3.2.2"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
